a554ecdcef3fccb91e733cc4275ff4a10e020425
DESKTOP-5LSDE5P\Miwtoo-NB <miwyugioh@gmail.com>
2018-07-05 00:50:48 +0700

    day 1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 new Vue({
-    el : '#app',
-    data : {
-        message : 'Hello Miwtoo'
+    el: '#app',
+
+    data: {
+        name: "Miwtoo",
+        job: "student",
+        age: 20,
+        website: "http://miwtoo.github.io/",
+        count: 0
+    },
+
+    methods: {
+        getName: function(){
+            return this.name
+        },
+        setName: function(n){
+            return this.name = n
+        },
+
+        addAge: function(){
+            this.age++
+        }
     }
 })
